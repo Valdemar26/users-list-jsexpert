6ea5ff61e49b11d276967c0d602bd0cdd6844e60
v.zaplatunskuj@crosssoft.de <102938qweasd>
2020-08-27 11:19:43 +0300

    1: lesson 1, project scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatListModule } from '@angular/material/list';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
